814b62b82f0dc0178d705019786f0f108861afa5
Sh4yn <guemmoudrayane03@gmail.com>
2022-05-05 01:08:34 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.scss';
+
+export default function App() {
+    return (
+        <div className="container">
+            <div className="naoki">
+                <h1>Naoki</h1>
+            </div>
+
+            <div className="content">
+                <img src="" alt="" />
+            </div>
+        </div>
+    )
+}
